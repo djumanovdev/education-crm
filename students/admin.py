9a02d8d17da4523e64e04a067aa30892f9c9b01a
Nodirbek <ablokulovnodirbek4@gmail.com>
2025-12-24 16:09:18 +0500

    feat: student-model

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Student,Teacher,Group,Attendance,Payment
 
-# Register your models here.
+admin.site.register([Student,Teacher,Group,Attendance,Payment])
